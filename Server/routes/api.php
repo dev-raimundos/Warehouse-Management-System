94341246515e2daa902c4856d3fc45cfbd12909f
dev-raimundos <devraimundos@outlook.com>
2025-08-24 19:51:38 -0300

    FEAT: Criando rota de testes

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/ping', function () {
+    return ['message' => 'pong'];
+});
+
